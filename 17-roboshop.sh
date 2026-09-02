2889a34206f1f0a72242194ef509bc94a1432eeb
manikantha <manikantha.mkn@gmail.com>
2026-09-02 20:02:25 +0530

    RoboShop

--- 17-roboshop.sh
+++ 17-roboshop.sh
@@ -10,7 +10,7 @@ for instance in ${INSTANCES}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-052c76aba2d33b868 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     
-    if[ $instance != "forntend" ]
+    if [ $instance != "forntend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0]. Instances[0].PrivateIpAddress" --output text)
     else
