c58458e81e01b7e5523fd8d1fdf33ee0f020154d
manikantha <manikantha.mkn@gmail.com>
2026-09-04 13:55:45 +0530

    Shell Practice

--- catalogue.sh
+++ catalogue.sh
@@ -77,5 +77,11 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-mongosh --host mongodb.arohvya.online </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loading data into MongoDB"
+STATUS=$(mongosh --host mongodb.arohvya.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.arohvya.online </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already Loaded ... $Y Skipping... $N"
+fi
